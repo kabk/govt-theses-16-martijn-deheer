d199323140841ceefefef7a5873f330653a27216
martijndeheer <martijndeheer@gmail.com>
2016-03-15 20:06:04 +0100

    o

---
+++ js/noteheight.js
@@ -0,0 +1,191 @@
+			setTimeout(function(){
+				noteHeight();
+			}, 3000);
+
+			// put notes on the right height
+			$(window).onresize = function(event){
+				noteHeight();
+			}
+
+			function noteHeight(){
+				var a = $( '[data-note="1"]' );
+				var offsetA =  a.offset();
+				$( '[data-content="1"]' ).offset({ top: offsetA.top });
+				$( '[data-content="1"]' ).attr('id', 'data-content-1')
+
+				var b = $( '[data-note="2"]' );
+				var offsetB =  b.offset();
+				$( '[data-content="2"]' ).offset({ top: offsetB.top });
+				$( '[data-content="2"]' ).attr('id', 'data-content-2')
+				var a = $( '[data-note="1"]' );
+
+				var c = $( '[data-note="3"]' );
+				var offsetC =  c.offset();
+				$( '[data-content="3"]' ).offset({ top: offsetC.top });
+				$( '[data-content="3"]' ).attr('id', 'data-content-3')
+
+				var d = $( '[data-note="4"]' );
+				var offsetD =  d.offset();
+				$( '[data-content="4"]' ).offset({ top: offsetD.top });
+				$( '[data-content="4"]' ).attr('id', 'data-content-4')
+
+				// var e = $( '[data-note="5"]' );
+				// var offsetE =  e.offset();
+				// $( '[data-content="5"]' ).offset({ top: offsetE.top });
+				// $( '[data-content="5"]' ).attr('id', 'data-content-5')
+
+				var f = $( '[data-note="6"]' );
+				var offsetF =  f.offset();
+				$( '[data-content="6"]' ).offset({ top: offsetF.top });
+				$( '[data-content="6"]' ).attr('id', 'data-content-6')
+
+				var g = $( '[data-note="7"]' );
+				var offsetG =  g.offset();
+				$( '[data-content="7"]' ).offset({ top: offsetG.top });
+				$( '[data-content="7"]' ).attr('id', 'data-content-7')
+
+				var h = $( '[data-note="8"]' );
+				var offsetH =  h.offset();
+				$( '[data-content="8"]' ).offset({ top: offsetH.top });
+				$( '[data-content="8"]' ).attr('id', 'data-content-8')
+
+				var i = $( '[data-note="9"]' );
+				var offsetI =  i.offset();
+				$( '[data-content="9"]' ).offset({ top: offsetI.top });
+				$( '[data-content="9"]' ).attr('id', 'data-content-9')
+
+				var j = $( '[data-note="10"]' );
+				var offsetJ =  j.offset();
+				$( '[data-content="10"]' ).offset({ top: offsetJ.top });
+				$( '[data-content="10"]' ).attr('id', 'data-content-10')
+
+				var k = $( '[data-note="11"]' );
+				var offsetK =  k.offset();
+				$( '[data-content="11"]' ).offset({ top: offsetK.top });
+				$( '[data-content="11"]' ).attr('id', 'data-content-11')
+
+				var l = $( '[data-note="12"]' );
+				var offsetL =  l.offset();
+				$( '[data-content="12"]' ).offset({ top: offsetL.top });
+				$( '[data-content="12"]' ).attr('id', 'data-content-12')
+
+				var m = $( '[data-note="13"]' );
+				var offsetM =  m.offset();
+				$( '[data-content="13"]' ).offset({ top: offsetM.top });
+				$( '[data-content="13"]' ).attr('id', 'data-content-13')
+
+				var n = $( '[data-note="14"]' );
+				var offsetN =  n.offset();
+				$( '[data-content="14"]' ).offset({ top: offsetN.top });
+				$( '[data-content="14"]' ).attr('id', 'data-content-14')
+
+				var o = $( '[data-note="15"]' );
+				var offsetO =  o.offset();
+				$( '[data-content="15"]' ).offset({ top: offsetO.top });
+				$( '[data-content="15"]' ).attr('id', 'data-content-15')
+
+				var p = $( '[data-note="16"]' );
+				var offsetP =  p.offset();
+				$( '[data-content="16"]' ).offset({ top: offsetP.top });
+				$( '[data-content="16"]' ).attr('id', 'data-content-16')
+
+				var q = $( '[data-note="17"]' );
+				var offsetQ =  q.offset();
+				$( '[data-content="17"]' ).offset({ top: offsetQ.top });
+				$( '[data-content="17"]' ).attr('id', 'data-content-17')
+
+				var r = $( '[data-note="18"]' );
+				var offsetR =  r.offset();
+				$( '[data-content="18"]' ).offset({ top: offsetR.top });
+				$( '[data-content="18"]' ).attr('id', 'data-content-18')
+
+				var s = $( '[data-note="19"]' );
+				var offsetS =  s.offset();
+				$( '[data-content="19"]' ).offset({ top: offsetS.top });
+				$( '[data-content="19"]' ).attr('id', 'data-content-19')
+
+				var t = $( '[data-note="20"]' );
+				var offsetT =  t.offset();
+				$( '[data-content="20"]' ).offset({ top: offsetT.top });
+				$( '[data-content="20"]' ).attr('id', 'data-content-20')
+
+				// var u = $( '[data-note="21"]' );
+				// var offsetU =  u.offset();
+				// $( '[data-content="21"]' ).offset({ top: offsetU.top });
+				// $( '[data-content="21"]' ).attr('id', 'data-content-21')
+
+				// var v = $( '[data-note="22"]' );
+				// var offsetV =  v.offset();
+				// $( '[data-content="22"]' ).offset({ top: offsetV.top });
+				// $( '[data-content="22"]' ).attr('id', 'data-content-22')
+
+				var w = $( '[data-note="23"]' );
+				var offsetW =  w.offset();
+				$( '[data-content="23"]' ).offset({ top: offsetW.top });
+				$( '[data-content="23"]' ).attr('id', 'data-content-23')
+
+				// var x = $( '[data-note="24"]' );
+				// var offsetX =  x.offset();
+				// $( '[data-content="24"]' ).offset({ top: offsetX.top });
+				// $( '[data-content="24"]' ).attr('id', 'data-content-24')
+
+				var y = $( '[data-note="25"]' );
+				var offsetY =  y.offset();
+				$( '[data-content="25"]' ).offset({ top: offsetY.top });
+				$( '[data-content="25"]' ).attr('id', 'data-content-25')
+
+				var z = $( '[data-note="26"]' );
+				var offsetZ =  z.offset();
+				$( '[data-content="26"]' ).offset({ top: offsetZ.top });
+				$( '[data-content="26"]' ).attr('id', 'data-content-26')
+
+				var aA = $( '[data-note="27"]' );
+				var offsetAA =  aA.offset();
+				$( '[data-content="27"]' ).offset({ top: offsetAA.top });
+				$( '[data-content="27"]' ).attr('id', 'data-content-27')
+
+				var aB = $( '[data-note="28"]' );
+				var offsetAB =  aB.offset();
+				$( '[data-content="28"]' ).offset({ top: offsetAB.top });
+				$( '[data-content="28"]' ).attr('id', 'data-content-28')
+
+				var aC = $( '[data-note="29"]' );
+				var offsetAC =  aC.offset();
+				$( '[data-content="29"]' ).offset({ top: offsetAC.top });
+				$( '[data-content="29"]' ).attr('id', 'data-content-29')
+
+				var aD = $( '[data-note="30"]' );
+				var offsetAD =  aD.offset();
+				$( '[data-content="30"]' ).offset({ top: offsetAD.top });
+				$( '[data-content="30"]' ).attr('id', 'data-content-30')
+
+				var aE = $( '[data-note="31"]' );
+				var offsetAE =  aE.offset();
+				$( '[data-content="31"]' ).offset({ top: offsetAE.top });
+				$( '[data-content="31"]' ).attr('id', 'data-content-31')
+
+				var aF = $( '[data-note="32"]' );
+				var offsetAF =  aF.offset();
+				$( '[data-content="32"]' ).offset({ top: offsetAF.top });
+				$( '[data-content="32"]' ).attr('id', 'data-content-32')
+
+				var aG = $( '[data-note="33"]' );
+				var offsetAG =  aG.offset();
+				$( '[data-content="33"]' ).offset({ top: offsetAG.top });
+				$( '[data-content="33"]' ).attr('id', 'data-content-33')
+
+				var aH = $( '[data-note="36"]' );
+				var offsetAH =  aH.offset();
+				$( '[data-content="36"]' ).offset({ top: offsetAH.top });
+				$( '[data-content="36"]' ).attr('id', 'data-content-36')
+
+				// var aH = $( '[data-note="34"]' );
+				// var offsetAH =  aH.offset();
+				// $( '[data-content="34"]' ).offset({ top: offsetAH.top });
+				// $( '[data-content="34"]' ).attr('id', 'data-content-34')
+
+				// var aI = $( '[data-note="35"]' );
+				// var offsetAI =  aI.offset();
+				// $( '[data-content="35"]' ).offset({ top: offsetAI.top });
+				// $( '[data-content="35"]' ).attr('id', 'data-content-35')
+			}
