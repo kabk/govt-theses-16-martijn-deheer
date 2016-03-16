73e8febcd420486359875c2f6739424c789d0769
martijndeheer <martijndeheer@gmail.com>
2016-03-16 16:10:07 +0100

    ff

--- js/fittext.js
+++ js/fittext.js
@@ -1,2 +1,8 @@
 	// fit header
 	$("#fittext").fitText(0.75, {minFontSize:'24px', maxFontSize:'180px'});
+
+	$('.content > p').children().each(function () {
+	    $(this).html(function (i, html) {
+	        return $(this).html().replace('&#160;', '·');
+	    });
+	});
--- js/print.js
+++ js/print.js
@@ -2,19 +2,7 @@
 
 document.addEventListener('layoutReady', function(){
 
-	// // fit header
-	// $("#fittext").fitText(0.75, {minFontSize:'24px'});
-
-	// $('.thesis-content > .content > p').children().each(function() {
-	// 	var textNode = $(this);
-	// 	textNode.text(textNode.text().replace("\xa0", "·"));
-	// });
-
-	$('.content > p').children().each(function () {
-	    $(this).html(function (i, html) {
-	        return $(this).html().replace('&#160;', '·');
-	    });
-	});
-
+	// fit header
+	$("#fittext").fitText(0.75, {minFontSize:'48px'});
 
 });
