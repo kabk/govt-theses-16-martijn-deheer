5c84f8c941b256ef17bbf9998add3506cf2df09b
martijndeheer <martijndeheer@gmail.com>
2016-03-16 17:17:32 +0100

    f

--- js/print.js
+++ js/print.js
@@ -5,7 +5,7 @@ document.addEventListener('layoutReady', function(){
 	// fit header
 	// $("#fittext").fitText(0.75, {minFontSize:'24px', maxFontSize:'180px'});
 
-	var div = $('#content');
+	var div = $('.content');
 	div.html(div.html().replace(/ /g, '·'));
 
 });
