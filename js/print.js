46d1ec2d3b9164168680e1c32b55c35ae2990843
martijndeheer <martijndeheer@gmail.com>
2016-03-16 15:54:22 +0100

    a

--- js/print.js
+++ js/print.js
@@ -5,9 +5,15 @@ document.addEventListener('layoutReady', function(){
 	// // fit header
 	// $("#fittext").fitText(0.75, {minFontSize:'24px'});
 
-	$('.thesis-content > .content > p').children().each(function() {
-		var textNode = $(this);
-		textNode.text(textNode.text().replace("\xa0", "·"));
+	// $('.thesis-content > .content > p').children().each(function() {
+	// 	var textNode = $(this);
+	// 	textNode.text(textNode.text().replace("\xa0", "·"));
+	// });
+
+	$('.content').children().each(function () {
+	    $(this).html(function (i, html) {
+	        return $(this).html().replace(/\xa0/g, '·');
+	    });
 	});
 
 
