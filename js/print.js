c76889ede3cdcc0c94f6aa1cb7bf6e3209837a9f
martijndeheer <martijndeheer@gmail.com>
2016-03-16 15:15:16 +0100

    l

--- js/print.js
+++ js/print.js
@@ -2,12 +2,10 @@
 
 document.addEventListener('layoutReady', function(){
 
-
-
 	// fit header
 	$("#fittext").fitText(0.75, {minFontSize:'24px'});
 
-	$('.content > p:eq(0)').children().each(function() {
+	$('.content > p').children().each(function() {
 		var textNode = $(this);
 		textNode.text(textNode.text().replace("\xa0", "·"));
 	});
