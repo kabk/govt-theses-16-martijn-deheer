b4e2e16868c2ba3d3684644ef95a33c18885974f
martijndeheer <martijndeheer@gmail.com>
2016-03-16 17:41:47 +0100

    f

--- js/print.js
+++ js/print.js
@@ -1,7 +1,11 @@
 
 document.addEventListener('layoutReady', function(){
 
-	var div = $('#article-wrapper');
-	div.html(div.html().replace(/ /g, '·'));
+	var div = $('#content');
+	var para = $('p');
+
+	$(para).each(function() {
+		div.html(div.html().replace(/ /g, '·'));
+	});
 
 });
