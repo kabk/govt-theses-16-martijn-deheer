4001f871f18ab96d70c17c7f98bc0a513f4b1f38
martijndeheer <martijndeheer@gmail.com>
2016-03-16 17:26:44 +0100

    a

--- js/print.js
+++ js/print.js
@@ -1,11 +1,7 @@
-$( document ).ready(function() {
-	var div = $('.thesis-content');
-	div.html(div.html().replace(/ /g, '·'));
-});
 
 document.addEventListener('layoutReady', function(){
 
-	// fit header
-	// $("#fittext").fitText(0.75, {minFontSize:'24px', maxFontSize:'180px'});
+	var div = $('.thesis-content');
+	div.html(div.html().replace(/ /g, '·'));
 
 });
