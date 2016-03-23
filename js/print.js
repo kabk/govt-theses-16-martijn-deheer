9ba8bad4ea5b331f669cea41d84f04ab5d2b510f
martijndeheer <martijndeheer@gmail.com>
2016-03-23 15:49:01 +0100

    -

--- js/print.js
+++ js/print.js
@@ -1,17 +1,17 @@
 
 document.addEventListener('layoutReady', function(){
 
-	findAndReplaceDOMText(document.body, {
-		preset: 'prose',
-		find: ' ',
-		replace: function(portion, match) {
-			called = true;
-			var el = document.createElement('em');
-			el.style.backgroundColor = "red";
-			el.innerHTML = portion.text;
-			return el;
-		}
-	});
+	// findAndReplaceDOMText(document.body, {
+	// 	preset: 'prose',
+	// 	find: ' ',
+	// 	replace: function(portion, match) {
+	// 		called = true;
+	// 		var el = document.createElement('em');
+	// 		el.style.backgroundColor = "red";
+	// 		el.innerHTML = portion.text;
+	// 		return el;
+	// 	}
+	// });
 
 
 });
