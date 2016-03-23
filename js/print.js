758f87c805bab4f21be2b91611a90d87811a931c
martijndeheer <martijndeheer@gmail.com>
2016-03-23 15:03:58 +0100

    g

--- js/print.js
+++ js/print.js
@@ -8,11 +8,16 @@ document.addEventListener('layoutReady', function(){
 	// 	div.html(div.html().replace(/ /g, '·'));
 	// });
 
-	findAndReplaceDOMText(document.getElementsByTagName('p'), {
-		find: / /g,
-		replace: '+'
-	});
-
-
+	findAndReplaceDOMText();
 });
 
+findAndReplaceDOMText(document.getElementsByTagName('p'), {
+	find: ' ',
+	replace: function(portion, match) {
+		called = true;
+		var el = document.createElement('em');
+		el.style.backgroundColor = colors[match.index % colors.length];
+		el.innerHTML = portion.text;
+		return el;
+	}
+});
