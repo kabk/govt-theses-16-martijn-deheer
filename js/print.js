e3a12b0e521907455f5e5a3e5472c8da245d9e0d
martijndeheer <martijndeheer@gmail.com>
2016-03-24 14:14:40 +0100

    0

--- js/print.js
+++ js/print.js
@@ -2,17 +2,17 @@
 document.addEventListener('layoutReady', function(){
 
 
-	findAndReplaceDOMText(document.body, {
-		preset: 'prose',
-		find: ' ',
-		replace: function(portion, match) {
-			called = true;
-			var el = document.createElement('em');
-			el.style.backgroundColor = "red";
-			el.innerHTML = portion.text;
-			return el;
-		}
-	});
+	// findAndReplaceDOMText(document.body, {
+	// 	preset: 'prose',
+	// 	find: ' ',
+	// 	replace: function(portion, match) {
+	// 		called = true;
+	// 		var el = document.createElement('em');
+	// 		el.style.backgroundColor = "red";
+	// 		el.innerHTML = portion.text;
+	// 		return el;
+	// 	}
+	// });
 
 
 });
